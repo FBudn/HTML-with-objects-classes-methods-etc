4989332eece86f576d17d8a1b5d7036a0b0dbd2b
FBudn <111660524+FBudn@users.noreply.github.com>
2022-11-13 12:35:34 +0100

    Added-4-objects

--- thisScript.js
+++ thisScript.js
@@ -21,12 +21,14 @@ document.getElementById("poka").innerHTML =
 
 // -------------------------------------------------- WŁAŚCIWY DOKUMENT
 
+//Metoda ze zmienną i object.create
+
 //OBJECT 1
 
 let object1Place = document.getElementById("forObject1");
 
 let object1 = {
-  name: "Obiekcik",
+  name: "Obiekt",
   surname: "Pierwszy",
   iDo: "Zwracam sumę liczb parzystych z tablicy",
   getEvenSum(a) {
@@ -34,4 +36,94 @@ let object1 = {
   },
 };
 
-object1Place.innerHTML += object1.name;
+object1Place.innerHTML +=
+  object1.name + " " + object1.surname + "<br>" + "<br>" + object1.iDo;
+
+//OBJECT 2
+
+let object2Place = document.getElementById("forObject2");
+
+let object2 = {
+  name: "Obiekcik",
+  surname: "Drugi",
+  iDo: "Kasuje swoje imię",
+  setDeleteNameProperty(a) {
+    delete a.name;
+    return a;
+  },
+};
+
+object2Place.innerHTML +=
+  object2.name + " " + object2.surname + "<br>" + "<br>" + object2.iDo;
+
+//OBJECT 3
+
+let object3Place = document.getElementById("forObject3");
+
+const object3 = {
+  name: "Obiekcik",
+  surname: "Trzeci",
+  iDo: "Odwracam słowa i liczby",
+  getReverse(a) {
+    return String(a).split("").reverse(a).join("");
+  },
+};
+
+object3Place.innerHTML +=
+  object3.name + " " + object3.surname + "<br>" + "<br>" + object3.iDo;
+
+//KLASY
+
+class Solgier {
+  constructor(job) {
+    this.job = "Żołnierz";
+  }
+}
+
+//Object 4
+
+class PolishGrot extends Solgier {
+  constructor(weapon, name, surname, country) {
+    super();
+    this.weapon = weapon;
+    this.name = name;
+    this.surname = surname;
+    this.country = country;
+  }
+
+  shoot() {
+    return "Boom Boom";
+  }
+}
+
+const solgierJan = new PolishGrot("Grot", "Jan", "Paweł", "Polska");
+
+let object4Place = document.getElementById("forObject4");
+
+object4Place.innerHTML +=
+  solgierJan.name +
+  " " +
+  solgierJan.surname +
+  "<br>" +
+  solgierJan.country +
+  "<br>" +
+  solgierJan.weapon +
+  "<br>" +
+  solgierJan.shoot();
+
+//Object 5
+
+class IranAK47 extends Solgier {
+  constructor(weapon, name) {
+    super();
+    this.weapon = weapon;
+    this.name = name;
+  }
+}
+
+let solgierAhmed = new Solgier("żołnierz") && new IranAK47("AK47", "Ahmed");
+
+let object5Place = document.getElementById("forObject5");
+
+object5Place.innerHTML +=
+  solgierAhmed.name + "<br>" + solgierAhmed.weapon + "<br>" + solgierAhmed.job;
