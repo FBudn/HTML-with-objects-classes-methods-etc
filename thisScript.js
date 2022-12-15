f63686608e8521db5ee016c13ff50eee0c06bc5f
FBudn <111660524+FBudn@users.noreply.github.com>
2022-12-15 11:23:07 +0100

    time-zone test

--- thisScript.js
+++ thisScript.js
@@ -345,7 +345,8 @@ plClockTime.innerHTML += plTime.format("HH:mm:ss");
 
 //London
 
-let b = moment.tz("2020-01-01 11:20", "Asia/Singapore"); //.tz("America/Toronto"); // "America/Toronto");
+//let b = moment.tz("2020-01-01 11:20", "Asia/Singapore"); //.tz("America/Toronto"); // "America/Toronto");
 let londonTime = document.getElementById("londonTime");
+let test = "test";
 
-londonTime.innerHTML += b.tz("America/Toronto").format("HH:mm:ss");
+londonTime.innerHTML += test; // b.tz("America/Toronto").format("HH:mm:ss");
